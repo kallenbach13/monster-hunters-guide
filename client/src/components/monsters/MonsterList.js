4a849f854a296cd437846d96d7eaaef6575ff104
Isak Kallenbach <kallenbach13@gmail.com>
2022-08-30 21:46:25 +0100

    changing up some client-side variable naming

--- client/src/components/monsters/MonsterList.js
+++ client/src/components/monsters/MonsterList.js
@@ -1,22 +1,21 @@
 import { useEffect, useState } from 'react'
-import ProductCard from './ProductCard'
+import MonsterCard from './MonsterCard'
 import { useDispatch, useSelector } from 'react-redux'
-import {  selectAllProducts, selectFetchAllProductsStatus } from '../../features/products/productsSlice'
-import { needsCheckoutRedirectUpdated } from '../../features/cart/cartSlice'
+import {  selectAllMonsters, selectFetchAllMonstersStatus } from '../../features/monsters/monstersSlice'
 import ReactPaginate from 'react-paginate'
 import './Pagination.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useHistory, useParams } from 'react-router-dom'
 
-const ProductList = () => {
+const MonsterList = () => {
   
-  const products = useSelector(selectAllProducts)
-  const productsStatus = useSelector(selectFetchAllProductsStatus)
+  const monsters = useSelector(selectAllMonsters)
+  const monstersStatus = useSelector(selectFetchAllMonstersStatus)
 
   const [data, setData] = useState([])
   const [perPage] = useState(8)
   const [pageCount, setPageCount] = useState(0)
-  const { productOffset = 0 }= useParams()
+  const { monsterOffset = 0 }= useParams()
   let history = useHistory()
   const dispatch = useDispatch()
 
@@ -25,14 +24,14 @@ const ProductList = () => {
   }, [dispatch])
 
   useEffect(() => {
-    const slice = Object.keys(products).slice(productOffset, productOffset + perPage)
+    const slice = Object.keys(monsters).slice(monsterOffset, monsterOffset + perPage)
     const postData = slice.map(keyName =>
-            <ProductCard
-              key={products[keyName].id}
-              product={products[keyName]} />)
+            <MonsterCard
+              key={monsters[keyName].id}
+              monster={monsters[keyName]} />)
     setData(postData)
-    setPageCount(Math.ceil(Object.keys(products).length / perPage))
-  }, [products, productOffset, perPage])
+    setPageCount(Math.ceil(Object.keys(monsters).length / perPage))
+  }, [monsters, monsterOffset, perPage])
 
   const handlePageClick = (e) => {
     const selectedPage = e.selected
@@ -43,14 +42,14 @@ const ProductList = () => {
   return (
             <div className="flex flex-col flex-grow">
    
-            { productsStatus === 'loading' && <FontAwesomeIcon  className="mt-20 mx-auto" icon="fas fa-spinner" size="4x" spin/>}
+            { monstersStatus === 'loading' && <FontAwesomeIcon  className="mt-20 mx-auto" icon="fas fa-spinner" size="4x" spin/>}
 
-              { productsStatus === 'failed' &&
+              { monstersStatus === 'failed' &&
                 <div className="p-4 mt-20 mx-auto max-w-screen-2xl">
                   <h2 className="text-lg text-center">Problem connecting with the server.</h2>
                 </div>}
 
-              { productsStatus === 'succeeded' &&
+              { monstersStatus === 'succeeded' &&
               <div className="flex flex-grow flex-col">
                 <div className="flex-grow">
                   <div className="p-4 flex flex-wrap justify-center max-w-screen-2xl mx-auto">
@@ -77,4 +76,4 @@ const ProductList = () => {
     )
   }
   
-  export default ProductList
+  export default MonsterList
---
+++ client/src/features/monsters/monstersSlice.js
@@ -0,0 +1,37 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import apiAxios from '../../config/axiosConfig'
+
+export const fetchAllMonsters = createAsyncThunk('monsters/fetchAllMonsters', async () => {
+        const response = await apiAxios.get('/monsters')
+        const monsters= {}
+        response.data.forEach(monster =>
+            monsters[monster.id] = monster)
+        return monsters
+})
+
+export const monstersSlice = createSlice({
+    name: 'monsters',
+    initialState: {
+        fetchAllMonstersStatus: 'idle',
+        allMonsters: {}
+    },
+    extraReducers: {
+        //Reducers for fetching monsters
+        [fetchAllMonsters.pending]: (state, action) => {
+            state.fetchAllMonstersStatus = 'loading'
+          },
+          [fetchAllMonsters.fulfilled]: (state, action) => {
+            state.fetchAllMonstersStatus = 'succeeded'
+            state.allMonsters = action.payload
+          },
+          [fetchAllMonsters.rejected]: (state, action) => {
+            state.fetchAllMonstersStatus = 'failed'
+          },
+    }
+})
+
+export const selectAllMonsters = state => state.monsters.allMonsters
+export const selectMonsterById = (state, monsterId) => state.monsters.allMonsters[monsterId]
+export const selectFetchAllMonstersStatus = state => state.monsters.fetchAllMonstersStatus
+
+export default monstersSlice.reducer
