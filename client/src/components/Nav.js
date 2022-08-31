9e36ddce830cc669f42cd7589dab6fe59504203d
Isak Kallenbach <kallenbach13@gmail.com>
2022-08-31 17:43:12 +0100

    adding favicon

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -35,7 +35,7 @@ function Nav() {
         <div className="max-w-8xl mx-auto flex flex-row justify-around items-center p-2">
               <div className="m-2">
                 <Link to="/">
-                  <p className="text-2xl md:text-3xl lg:text-4xl text-pink-300 text-center font-serif whitespace-nowrap mx-2">streetweary.</p>
+                  <p className="text-2xl md:text-3xl lg:text-4xl text-pink-300 text-center font-serif whitespace-nowrap mx-2">Monster Hunter's Guide</p>
                 </Link>
               </div>
 
